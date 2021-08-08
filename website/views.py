8f76e224bdb1798e40e689c3e557f5072b7e5680
sweem2k <mmd369@sbcglobal.net>
2021-08-08 17:17:51 -0400

    init

---
+++ website/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template
+import json
+
+views = Blueprint("views", __name__)
+
+@views.route("/")
+@views.route("/home")
+def home():
+    highspeed = openjson('website/static/highspeed.json')
+    
+    return render_template("base.html", data = highspeed)
+    
+def openjson(filename):
+    # Opening JSON file
+    f = open(filename)
+  
+    # returns JSON object as a dictionary
+    # a dictionary
+    data = json.load(f)
+  
+    #close file
+    f.close()
+    
+    return data
